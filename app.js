3a0109d8ba17fe0f002ed1913f1049c047b4aedc
Ivan Vetrov <C:\Users\Ivan\AppData\Roaming\The Bat!>
2013-11-16 07:27:14 +0300

    first

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
+function mobileDetect() {
+  var mobileOS;
+  var agent = navigator.userAgent.toLowerCase();
+  if (agent.match(/ipad/i) || agent.match(/iphone/i)) {
+    mobileOS = 'ios';
+  }
+  else if (agent.match(/android/i)) {
+    mobileOS = 'android';
+  }
+  else {
+    mobileOS = 'unknown';
+  }
+  return mobileOS;
+}
+
 airomo.setEnv({clientId:582169394, apiKey:'k60vg92ehyplqi99fhlnz1bpbxgmvudw'});
 
 (function() {
+  var mob = mobileDetect();
   var appTemplate = '<li>' +
     '<div class="overlay"> <a href="{{app_url}}"><img style="width:70px;height:70px;" src="{{app_icon}}" alt="" />' +
     '<div></div>' +
@@ -9,7 +25,14 @@ airomo.setEnv({clientId:582169394, apiKey:'k60vg92ehyplqi99fhlnz1bpbxgmvudw'});
     '<h6><a href="{{app_url}}">{{app_title}}</a></h6>' +
     '<em>{{app_descr}}</em> </div>' +
     '</li>';
-  airomo.search({query:'porsche', size: 4}, function(err, data) {
+  var query = {query:'porsche', size: 4};
+  if(mob === 'ios') {
+    query.store = 1;
+  }
+  else if(mob === 'android') {
+    query.store = 2;
+  }
+  airomo.search(query, function(err, data) {
     if(!err) {
       $('#porsche-apps').html('');
       $.each(data.results, function(i, e) {
@@ -30,11 +53,20 @@ airomo.setEnv({clientId:582169394, apiKey:'k60vg92ehyplqi99fhlnz1bpbxgmvudw'});
 
 
 (function() {
+  var mob = mobileDetect();
   var appTemplate = '<div class="search-item">' +
     '<a href="{{app_url}}">' +
     '<img style="width:100%;height:100%;" src="{{app_icon}}" alt="" />' +
     '</a></div>';
-  airomo.search({query:'classic cars', size: 12}, function(err, data) {
+  
+  var query = {query:'classic cars', size: 12};
+  if(mob === 'ios') {
+    query.store = 1;
+  }
+  else if(mob === 'android') {
+    query.store = 2;
+  }
+  airomo.search(query, function(err, data) {
     if(!err) {
       $('#classic-cars').html('');
       $.each(data.results, function(i, e) {
@@ -55,6 +87,13 @@ airomo.setEnv({clientId:582169394, apiKey:'k60vg92ehyplqi99fhlnz1bpbxgmvudw'});
 
 function doSearch() {
   var platform = '';
+  var mob = mobileDetect();
+  if(mob === 'ios') {
+    platform = '&stores=1';
+  }
+  else if(mob === 'android') {
+    platform = '&stores=1';
+  }
   var keywords = ' classic cars';
   var url = 'http://www.appcurl.com/?q=' + encodeURIComponent($('#s2').val() + keywords) + platform;
   window.open(url);
