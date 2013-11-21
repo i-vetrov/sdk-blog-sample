4085b538bd8612962e73f645e29613fdfb9b5b4b
Ivan Vetrov <C:\Users\Ivan\AppData\Roaming\The Bat!>
2013-11-21 09:26:12 +0300

    update

--- s-app/app.js
+++ s-app/app.js
@@ -6,20 +6,22 @@ var currentQuery = '';
 var appTemplate =  '<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 main-app-holder">' +
   '<div class="row app-data-row">' +
   '<div class="app-icon-holder">' +
+  '<a href="{{applink}}" target="_blank">' +
   '<img src="{{icon}}" class="app-icon" />' +
+  '</a>' +
   '</div>' +
   '<div class="app-data-text" style="">' +
-  '<div class="app-title">{{title}}</div>' +
+  '<div class="app-title"><a href="{{applink}}" target="_blank">{{title}}</a></div>' +
   '<div class="app-category">{{category}}</div>' +
   '<div class="app-rating">' +
   '<span class="rating-num">{{rating}}</span>' +
   '<div class="stars-holder">' +
-  '{{stars}}'+
+  '<span class="glyphicon glyphicon-star rating-star"></span>' +
   '</div>' +
   '</div>' +
   '<div class="app-data">' +
-  '<span class="app-store"></span>' +
-  '<button class="btn btn-default btn-lg app-price">{{price}}</button>' +
+  '<span class="app-store"><i class="fa fa-{{storeicon}} store-icon"></i></span>' +
+  '<button onclick="install(\'{{applink}}\')" class="btn btn-default btn-lg app-price {{free}}">{{price}}</button>' +
   '</div> ' +
   '</div>' +
   '<div class="app-data-stats">' +
@@ -46,7 +48,7 @@ var appTemplate =  '<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 main-app
   '</div>' +
   '</div>' +
   '</div>';
-var starTemplate = '<span class="glyphicon glyphicon-star rating-star"></span>';
+
 var catList = ['', 'Books & Reference', 'Business', 'Education', 'Entertainment', 
   'Finance', 'Games', 'Health & Fitness', 'Lifestyle', 'Media & Video', 'Medical', 
   'Music & Audio', 'Navigation', 'News & Magazines', 'Photography', 'Productivity', 
@@ -54,39 +56,49 @@ var catList = ['', 'Books & Reference', 'Business', 'Education', 'Entertainment'
   'Personalization'];
 
 function renderResults(results) {
+  console.log(results)
   $.each(results, function(i, e) {
-    e.stars = '';
-    var priceAlias = 'Free'
-    for(var z = 0; z < Math.floor(e.rating); z++) {
-      e.stars += starTemplate;
+    var storeIcon;
+    if(e.store == 1) {
+      storeIcon = 'apple';
     }
+    else {
+      storeIcon = 'android';
+    }
+    e.stars = '';
+    var priceAlias = 'Free';
+    var free = 'btn-red';
     if(e.price !== '0.00') {
       priceAlias = '$' + e.price;
-      
+      free = '';
     }
     var curApp = appTemplate
-      .replace('{{title}}', e.title)
+      .replace(new RegExp('{{applink}}', 'g'), e.nativeUrl)
+      .replace('{{title}}', e.title.length > 30 ? e.title.substring(0,30) + '...' : e.title)
       .replace('{{icon}}', e.icon)
       .replace('{{category}}', catList[e.category.pop() || 0])
       .replace('{{rating}}', e.rating.toString().substring(0, 3))
-      .replace('{{stars}}', e.stars)
+      .replace('{{storeicon}}', storeIcon)
       .replace('{{price}}', priceAlias)
+      .replace('{{free}}', free)
       .replace('{{shares}}', e.shares || 0)
       .replace('{{reviews}}', e.totalReview)
       .replace('{{likes}}', e.likes || 0)
       .replace('{{description}}', e.description.substring(0, 200) + '...')
       .replace('{{screenshot}}', e.screenshots[0] || '')
       if(!!(i % 2)) {
-        $('#left').append(curApp);
+        $('#right').append(curApp);
       }
       else {
-        $('#right').append(curApp);
+        $('#left').append(curApp);
       }
       $('#single').append(curApp);
   });   
 }
 
 function doSearch() {
+  $('.search-text-icon').hide();
+  $('.search-button-loader').show();
   var query = $('#query-input').val();
   currentQuery = query;
   $('#left').html('');
@@ -102,6 +114,8 @@ function doSearch() {
         $('#more-res').show();
       }
       renderResults(data.results);
+      $('.search-text-icon').show();
+      $('.search-button-loader').hide();
     }
     else {
       console.log('Something went wrong...', err);
@@ -110,6 +124,8 @@ function doSearch() {
 }
 
 function nextPage() {
+  $('.search-text-icon').hide();
+  $('.search-button-loader').show();
   airomo.search({query:currentQuery, size: pageSize, offset:pageSize*page}, function(err, data) {
     if(!err) {
       page++;
@@ -117,9 +133,21 @@ function nextPage() {
       if(totalResults<pageSize*page) {
         $('#more-res').hide();
       }
+      $('.search-text-icon').show();
+      $('.search-button-loader').hide();
     }
     else {
       console.log('Something went wrong...', err);
     }
   });
+}
+
+function install(url) {
+  window.open(url);
+}
+
+function keyListener(event) {
+  if(event.keyCode == 13){
+    doSearch();
+  }
 }
